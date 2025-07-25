083ec4640a806011e9ad0275ed6238adb21d21c9
e-yuma <e.yuma.jp@gmail.com>
2025-07-25 21:17:41 +0900

    no message

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,13 +4,19 @@ const Hero: React.FC = () => {
   return (
     <section
       id="hero"
-      className="fixed inset-0 w-full h-screen flex items-center justify-center overflow-hidden z-0"
+      className="fixed inset-0 w-full h-lvh flex items-center justify-center overflow-hidden z-0"
     >
       <img
         className="w-full h-full object-cover"
         src="/assets/hero.png"
         alt="hero"
       />
+      <div className="absolute left-8 text-lg tracking-widest font-light rotate-[90deg] origin-bottom-left">
+        Instagram
+      </div>
+      <div className="absolute bottom-8 left-8 text-5xl tracking-widest font-semibold">
+        YOUTUBE
+      </div>
     </section>
   );
 };
